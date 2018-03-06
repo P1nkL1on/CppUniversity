3d6fae587ac76569ab76043065abf4ca0f661c14
Leonid <lion6149@mail.ru>
2018-03-06 07:39:52 +0400

    chimics done

--- Chimestry/main.cpp
+++ Chimestry/main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     Atom a = Atom("aa", 3, 2);
     a.Trace();
+
+    Molecule H2O = Molecule("water", 3, new Atom*[3]{new Atom("H ", 1, 1), new Atom("O ", 16, 8), new Atom("H ", 1, 1)});
+    H2O.Trace();
+
     return 0;
 //    Molecule H2O = Molecule("water", 3, new Atom[3] {Atom("H ", 1, 1), Atom("O ", 16, 8), Atom("H ", 1, 1)});
 //    H2O.Trace();
--- Chimestry/molecule.cpp
+++ Chimestry/molecule.cpp
@@ -6,8 +6,9 @@ Molecule::Molecule()
     atomCount = 0;
 }
 
-Molecule::Molecule(char* molName, int molAtomCount, Atom* molAtoms)
+Molecule::Molecule(char* molName, int molAtomCount, Atom** molAtoms)
 {
+    cout << "+ " << molName << endl;
     atoms = molAtoms;
     atomCount = molAtomCount;
     name = molName;
@@ -15,21 +16,25 @@ Molecule::Molecule(char* molName, int molAtomCount, Atom* molAtoms)
 
 Molecule::~Molecule()
 {
-
+    delete[] atoms;
+    cout << "~ " << name << endl;
 }
 
 void Molecule::Trace() const{
     int ac = -1;
-    cout << "Name: " <<name  << endl << "  Formula: ";
+
+    cout << name  << endl << "  Mass: " << getMass() <<  endl << "  e- : " << getElectronCount() << endl;
+    cout << "  p+ :  " << getProtonCount() << endl << "  n0 :  " << getNeironCount() << endl;
+    cout << endl << "Formula: " << endl;
 
     char typesWas[18];
 
     while (atomCount - (ac++) - 1)
-        if (typesWas[atoms[ac].getElectronCount() - 1] != 'y')
+        if (typesWas[atoms[ac]->getElectronCount() - 1] != 'y')
         {
-            typesWas[atoms[ac].getElectronCount() - 1] = 'y';
-            int count = CountAtomType(atoms[ac].getElectronCount());
-            char* name = atoms[ac].getName();
+            typesWas[atoms[ac]->getElectronCount() - 1] = 'y';
+            int count = CountAtomType(atoms[ac]->getElectronCount());
+            char* name = atoms[ac]->getName();
 
             if (name[1] == ' ') cout << name[0]; else cout << name;
             if (count != 1)cout << count;
@@ -37,10 +42,10 @@ void Molecule::Trace() const{
     cout << endl << "  e-: " << getElectronCount() << endl << "  Mass: " << getMass() <<  endl << "Atoms:" << endl;
     ac = -1;
     while (atomCount - (ac++) - 1)
-        if (typesWas[atoms[ac].getElectronCount() - 1] != 'n'){
-            typesWas[atoms[ac].getElectronCount() - 1] = 'n';
-            cout << CountAtomType(atoms[ac].getName()) << " X ";
-            atoms[ac].Trace();
+        if (typesWas[atoms[ac]->getElectronCount() - 1] != 'n'){
+            typesWas[atoms[ac]->getElectronCount() - 1] = 'n';
+            cout << CountAtomType(atoms[ac]->getName()) << " X ";
+            atoms[ac]->Trace();
         }
 
 }
@@ -50,32 +55,32 @@ int Molecule::CountAtomType (int type) const{
 }
 int Molecule::CountAtomType (char* type) const{
     int ac = atomCount, summ = 0;
-    while (ac--)
-        summ += 1 * (atoms[ac].getName()[0] == type[0] && atoms[ac].getName()[1] == type[1]);
+    while (--ac>=0)
+        summ += 1 * (atoms[ac]->getName()[0] == type[0] && atoms[ac]->getName()[1] == type[1]);
     return summ;
 }
 
 
 int Molecule::getElectronCount () const{
     int ac = atomCount, summ = 0;
-    while (ac--) summ += atoms[ac].getElectronCount();
+    while (--ac>=0) summ += atoms[ac]->getElectronCount();
     return summ;
 }
 int Molecule::getProtonCount () const{
     int ac = atomCount, summ = 0;
-    while (ac--) summ += atoms[ac].getProtonCount();
+    while (--ac>=0) summ += atoms[ac]->getProtonCount();
     return summ;
 }
 int Molecule::getNeironCount () const{
     int ac = atomCount, summ = 0;
-    while (ac--) summ += atoms[ac].getNeironCount();
+    while (--ac>=0) summ += atoms[ac]->getNeironCount();
     return summ;
 }
 
 int Molecule::getMass() const
 {
     int ac = atomCount, summ = 0;
-    while (ac--) summ += atoms[ac].getMass();
+    while (--ac>=0) summ += atoms[ac]->getMass();
     return summ;
 }
 
