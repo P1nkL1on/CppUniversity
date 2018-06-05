f9940f0262bc20b850e2d0180a952c047391309d
Leonid <lion6149@mail.ru>
2018-06-05 23:29:02 +0300

    log generator

---
+++ LOGgenerator/LogType.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGgenerator
+{
+    public static class LogConsts
+    {
+        static Random rnd = new Random();
+        static string[] months = new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        static string[] methods = new string[] { "GET", "POST", "ADD", "PUSH", "LOL" };
+        static string[] adresses = new string[] { "/foo/", "/fo/", "/done/", "/serv/", "/tmp/", "/clnt/" };
+        static string nameCan = "1234567890qwertyuiopasdfghjklzxcvbnm";
+        static string[] backs = new string[] { ".ru", ".com", ".html", ".en" };
+        static string[] tags = new string[] { "cont", "temp", "time", "date", "cs", "vsu", "keka", "tutel", "prohor", "kodgen", "unity", "utility", "process", "java", "is", "so", "cool", "cpp" };
+
+        public static string getAdress()
+        {
+            string res = adresses[rnd.Next(adresses.Length)];
+            int leng = rnd.Next(3, 20);
+            while (leng-- > 0) res += nameCan[rnd.Next(nameCan.Length)];
+            res += backs[rnd.Next(backs.Length)];
+            return res;
+        }
+
+        public static List<String> gethistory(int length)
+        {
+            List<String> res = new List<string>();
+            if (length < 0)
+                length = rnd.Next(5, 100);
+            int time = 0;
+            for (int i = 0; i < length; i++)
+            {
+                time += rnd.Next(20, 60 * 10);
+                res.Add((time / 60 / 60 + ":" + (time / 60 % 60) + ":" + (time % 60) + " " + methods[rnd.Next(methods.Length)]));
+            }
+            return res;
+        }
+
+        public static string getVersion()
+        {
+            return String.Format("#Version: {0}.{1}", rnd.Next(1, 10), rnd.Next(0, 1000));
+        }
+        public static string getDate()
+        {
+            return String.Format("#Date: {0}-{1}-{2} {3}:{4}:{5}", rnd.Next(28) + 1, months[rnd.Next(12)], 1990 + rnd.Next(28), rnd.Next(24).ToString().PadLeft(2, '0'), rnd.Next(60).ToString().PadLeft(2, '0'), rnd.Next(60).ToString().PadLeft(2, '0'));
+        }
+        public static string getFields()
+        {
+            String res = "";
+            int length = rnd.Next(2, 10);
+            for (int i = 0; i < length; i++)
+            {
+                res += " " + tags[rnd.Next(tags.Length)];
+            }
+            return String.Format("#Fields:{0}", res);
+        }
+        public static string makeRandomLog()
+        {
+            string result = getVersion() + "\r\n" + getDate() + "\r\n" + getFields() + "\r\n";
+            string adress = getAdress();
+            foreach (String line in gethistory(-1)){
+                result += line + " " + adress + "\r\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ LOGgenerator/WriterToFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace LOGgenerator
+{
+    public static class WriterToFile
+    {
+        public static void writedown (List<string> what){
+            // Create a string array with the lines of text
+
+            // Set a variable to the My Documents path.
+            string mydocpath =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            int numer = 0;
+            foreach (string log in what)
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(mydocpath,"log_"+(++numer)+".txt"))) {
+                    outputFile.WriteLine(log);
+            }
+        }
+    }
+}
