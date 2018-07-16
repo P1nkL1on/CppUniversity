99851091423dcee52f83b5da3f0bea55519da3eb
Leonid <lion6149@mail.ru>
2018-07-17 00:01:49 +0300

    More arrow navigation (now cute)

--- MTGhandler/Program.cs
+++ MTGhandler/Program.cs
@@ -15,17 +15,30 @@ namespace MTGhandler
             MDrawHandler.MaximiseWindow();
             CColor red = new CColor(ConsoleColor.Red);
 
-
             MLayoutHorizontal test = new MLayoutHorizontal(4);
-            MLayoutVertical testV = new MLayoutVertical(1), testL = new MLayoutVertical(3);
+            MLayoutVertical testV = new MLayoutVertical(1), testL = new MLayoutVertical(3), testL2 = new MLayoutVertical(1);
             test.AddWidget(new MLable("okay", 2));
+            for (int i = 0; i < 7; ++i)
+                if (i % 2 == 0)
+                    testL2.AddWidget(new MCheckBox("Okay" + i));
+                else
+                    testL2.AddWidget(new MTestWidget());
+                for (int i = 0; i < 3; ++i)
+                    test.AddWidget(new MTestWidget());
             for (int i = 0; i < 3; ++i)
-                test.AddWidget(new MTestWidget());
-            for (int i = 0; i < 6; ++i)
                 testL.AddWidget(new MLable("Line number " + i * i * i));
+            testL.AddWidget(new MListBox(10, -1, new List<MLineWidget>(){
+                new MCheckBox("AAA", false),
+                new MCheckBox("BBBBBB", false),
+                new MCheckBox("CCC", false)}));
+            for (int i = 0; i < 3; ++i)
+                testL.AddWidget(new MLable("Line number " + i * i * i * (-2)));
             test.AddWidget(testL);
             for (int i = 0; i < 3; ++i)
                 test.AddWidget(new MTestWidget());
+            testL.AddWidget(new MListBox(10, -1, new List<MLineWidget>(){
+                new MCheckBox("AaA", false),
+                new MCheckBox("BbBbBb", false)}));
             test.AddWidget(new MListBox(20, -1, new List<MLineWidget>(){
                 new MLable("---A---"),
                 new MCheckBox("A1", false),
@@ -36,8 +49,9 @@ namespace MTGhandler
                 new MCheckBox("C2", true),
                 new MCheckBox("C3", true),
                 }));
-            for (int i = 0; i < 2; ++i)
-                test.AddWidget(new MTestWidget());
+            test.AddWidget(new MTestWidget());
+            test.AddWidget(testL2);
+            test.AddWidget(new MTestWidget());
             for (int i = 0; i < 3; ++i)
                 testV.AddWidget(new MTestWidget());
 
@@ -54,6 +68,7 @@ namespace MTGhandler
             testV.Controller.SendEvent(MEvent.RedrawEvent(new MPoint(4, 10), testV));
 
             testV.Controller.SendEvent(MEvent.UnlockEvent(testV));
+            testV.Redraw();
 
             Logs.Write = true;
             while (true)
